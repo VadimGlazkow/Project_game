a6f614683a3c15f7a72cb9bb390364aa082d621b
Вадим <glazkow.14.vadim@yandex.ru>
2020-12-30 14:51:47 +0300

    Добавил спавн

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ tile_images = {
             'stone': pygame.transform.scale(load_image('stone.png', colorkey=-1), (100, 100)),
             'grass': pygame.transform.scale(load_image('grass.png', colorkey=-1), (100, 100)),
             'home': pygame.transform.scale(load_image('home.jpg', colorkey=1), (200, 200)),
+            'spawn': pygame.transform.scale(load_image('spawn.png', colorkey=1), (200, 300)),
             'flower_one': pygame.transform.scale(load_image('flower_one.png', colorkey=1), (50, 50)),
             'flower_two': pygame.transform.scale(load_image('flower_two.png', colorkey=1), (50, 50)),
             'flower_three': pygame.transform.scale(load_image('flower_three.png', colorkey=1), (50, 50)),
@@ -42,6 +43,7 @@ tile_images = {
         }
 player_image = pygame.transform.scale(load_image('mar.png'), SIZE_HERO)
 tile_width = tile_height = 100
+SPEDD = tile_width // 25
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
@@ -77,7 +79,8 @@ class Player(pygame.sprite.Sprite):
         for sprite in tiles_group:
             if pygame.sprite.collide_mask(self, sprite):
                 if sprite.image in (tile_images["stone"], tile_images["tree"],
-                                    tile_images["fence"], tile_images["home"]):
+                                    tile_images["fence"], tile_images["home"],
+                                    tile_images["spawn"]):
                     collect = True
                     break
         if collect:
@@ -90,25 +93,25 @@ class Player(pygame.sprite.Sprite):
         if pygame.sprite.spritecollideany(self, tiles_group):
             self.rect.x -= new_x
         else:
-            self.rect.x -= new_x + tile_width // 25
+            self.rect.x -= new_x + SPEDD
 
         self.rect.x -= new_x
         if pygame.sprite.spritecollideany(self, tiles_group):
             self.rect.x += new_x
         else:
-            self.rect.x += new_x + tile_width // 25
+            self.rect.x += new_x + SPEDD
 
         self.rect.y += new_y
         if pygame.sprite.spritecollideany(self, tiles_group):
             self.rect.y -= new_y
         else:
-            self.rect.y -= new_y + tile_height // 25
+            self.rect.y -= new_y + SPEDD
 
         self.rect.y -= new_y
         if pygame.sprite.spritecollideany(self, tiles_group):
             self.rect.y += new_y
         else:
-            self.rect.y += new_y + tile_height // 25
+            self.rect.y += new_y + SPEDD
 
 
 class Camera:
@@ -159,6 +162,8 @@ def generate_level(level):
                 new_player = Player(x, y)
             elif level[y][x] == '&':
                 Tile('home', x, y)
+            elif level[y][x] == '-':
+                Tile('spawn', x, y)
     return new_player
 
 
@@ -217,10 +222,10 @@ def game(level):
     fon = pygame.transform.scale(pygame.image.load('fon.jpg'), (WIDTH, HEIGHT))
     screen.blit(fon, (0, 0))
     camera = Camera()
-    dict_go = {"left": [False, [-tile_width // 25, 0]],
-               "right": [False, [tile_width // 25, 0]],
-               "up": [False, [0, -tile_height // 25]],
-               "down": [False, [0, tile_height // 25]]}
+    dict_go = {"left": [False, [-SPEDD, 0]],
+               "right": [False, [SPEDD, 0]],
+               "up": [False, [0, -SPEDD]],
+               "down": [False, [0, SPEDD]]}
     list_side = ['left', "right", "up", "down"]
     player = generate_level(level)
     while True:
